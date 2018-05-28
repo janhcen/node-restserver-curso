1564a4f5cf4d79f00f7ecc333bb972f9c648aced
John <j.anhuaman@gmail.com>
2018-05-27 23:23:12 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,14 @@
 
 process.env.PORT = process.env.PORT || 3000
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+let urlDB
+
+if (process.env.NODE_ENV === 'dev') {
+  urlDB = 'mongodb://localhost:27017/jcafe'
+} else {
+  urlDB = 'mongodb://jcafe-user:123456@ds137740.mlab.com:37740/jcafe'
+}
+
+process.env.URLDB = urlDB
--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 require('./config/config');
 
-const express = require('express');
-const app = express();
+const express = require('express')
+const mongoose = require('mongoose')
 
-const bodyParser = require('body-parser');
+const app = express()
+
+const bodyParser = require('body-parser')
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -11,43 +13,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(require('./routes/usuario'))
 
-app.get('/usuario', function (req, res) {
-  res.json('get Usuario LOCAL!!!');
-});
 
-app.post('/usuario', function (req, res) {
+mongoose.connect(process.env.URLDB, (err, res) => {
+  if (err) throw err
 
-  let body = req.body;
-
-  if (body.nombre === undefined) {
-
-    res.status(400).json({
-      ok: false,
-      mensaje: 'El nombre es necesario'
-    });
-
-  } else {
-    res.json({
-      persona: body
-    });
-  }
-
-});
-
-app.put('/usuario/:id', function (req, res) {
-
-  let id = req.params.id;
-
-  res.json({
-    id
-  });
-});
-
-app.delete('/usuario', function (req, res) {
-  res.json('delete Usuario');
-});
+  console.log('DB local loaded')
+})
 
 app.listen(process.env.PORT, () => {
-  console.log('Running on port: ', process.env.PORT);
-});
+  console.log('Running on port: ', process.env.PORT)
+})
